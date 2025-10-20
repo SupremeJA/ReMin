d09aed91813dcb885984665719cc23cf7a0e4bc4
Joshua Afolabi <telvinamin15@gmail.com>
2025-10-20 04:13:38 +0100

    centered featured title

--- src/sections/Featured.jsx
+++ src/sections/Featured.jsx
@@ -35,7 +35,7 @@ const Featured = () => {
 
   return (
     <section className="py-12">
-      <h1 className="title mb-8">Stay on track, effortlessly</h1>
+      <h1 className="title mb-8 text-center">Stay on track, effortlessly</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {cards.map((c, i) => (
